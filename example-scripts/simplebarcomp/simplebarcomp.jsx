b43ccba58a470b12d3c2509b30cd2529f05d8a76
Bill Kidwell <bkidwell@opentext.com>
2018-04-20 11:28:34 -0400

    Simple bar as a component

---
+++ example-scripts/simplebarcomp/simplebarcomp.jsx
@@ -0,0 +1,39 @@
+d3.selection.prototype.appendJSX = d3.jsx_append
+
+const data = [
+  {name: "Locke",    value:  4},
+  {name: "Reyes",    value:  8},
+  {name: "Ford",     value: 15},
+  {name: "Jarrah",   value: 16},
+  {name: "Shephard", value: 23},
+  {name: "Kwon",     value: 42}
+];
+
+const SimpleBars = (props) => {
+
+  const barHeight = props.height | 50;
+
+  return (
+    <g transform={(d, i) => { return 'translate(0,' + i*barHeight + ')';}}>
+      <rect width={(d) => x(d.value)} height={barHeight - 1}></rect>
+      <text x={(d)=>x(d.value)-10} y={ barHeight / 2 } dy=".35em">{(d) => d.value}</text>
+    </g>    
+  )
+}
+
+
+const width = 1000;
+
+const height = 800;      
+
+const x = d3.scaleLinear()
+          .range([0, width])
+          .domain([0, d3.max(data, (d) => d.value)]);                       
+
+let svg = d3.select("body").appendJSX(<svg className="chart" width={width} height={height}></svg>);
+
+
+const bar = svg.selectAll("g")
+.data(data)
+.enter()
+  .appendJSX((<SimpleBars height={40} /> ));
